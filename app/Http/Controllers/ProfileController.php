1bd00deef9f197369154c7b8b2d6ba9ad6b1bd17
cy322666 <cy322666@gmail.com>
2021-10-21 18:10:45 +0300

    [build] add relation + logic prepare get profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Profile;
+use App\Services\amoCRM\Helpers\Contacts;
+use App\Services\amoCRM\Helpers\Leads;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
@@ -9,12 +12,45 @@ class ProfileController extends Controller
     public function profiles()
     {
         //запрос профилей
-
+        //фильтр последняя транзация 40 дней назад
         //раскидываем по бд
     }
 
+    //выполняем работу с амо
     public function send()
     {
-        //выполняем работу с амо
+        $profiles = Profile::where('status', 'Добавлено')->all();
+
+        if($profiles->count() > 0) {
+
+            foreach ($profiles as $profile) {
+
+                $contact = Contacts::find($profile);
+
+                if($contact) {
+
+                    $leads = Leads::search($contact, $this->amocrm, env('PIPELINE'));//TODO pipeline?
+
+                    if(count($leads) > 0) {
+
+                        foreach ($leads as $lead) {
+
+                            if($lead->status_id) 'sad';//TODO логика для этапов
+                        }
+
+                    } else {
+
+                        //не найдено активных
+                        //надо создать новую в УР
+                    }
+
+                } else {
+
+                    //не найдено контакта
+                    //надо создать
+
+                }
+            }
+        }
     }
 }
--- app/Services/amoCRM/Helpers/Leads.php
+++ app/Services/amoCRM/Helpers/Leads.php
@@ -10,86 +10,30 @@ use Illuminate\Support\Facades\Log;
 
 abstract class Leads
 {
-    public static function search($contact, $client, int $pipeline_id = null)
+    //поиск активных в воронке
+    public static function search($contact, $client, int $pipeline_id = null) : array
     {
+        $leads = [];
+
         if($contact->leads) {
 
-            foreach ($contact->leads->toArray() as $lead) {
+            foreach ($contact->leads as $lead) {
 
-                if ($lead['status_id'] != 143 &&
-                    $lead['status_id'] != 142) {
+                if ($lead->status_id != 143 &&
+                    $lead->status_id != 142) {
 
-                    if($pipeline_id != null && $lead['pipeline_id'] == $pipeline_id) {
+                    if($pipeline_id != null && $lead->pipeline_id == $pipeline_id) {
 
                         $lead = $client->service
                             ->leads()
-                            ->find($lead['id']);
+                            ->find($lead->id);
 
-                        return $lead;
+                        $leads = array_merge($leads, $lead);
                     }
                 }
             }
         }
-        return null;
-    }
-
-    /**
-     * @param $statuses array массив статусов для поиска в порядке приоритета
-     * @param $responsible_user_id integer ответственный для проверке в сделке
-     */
-    public static function searchByStatus($amoApi, $statuses_pipelines, int $responsible_user_id)
-    {
-        $arrayLeads = [];
-
-        foreach ($statuses_pipelines as $statuses_pipeline) {
-
-            if($statuses_pipeline['status_id'] == '') continue;
-            
-            $query = [
-                'filter[statuses][0][status_id]'   => $statuses_pipeline['status_id'],
-                'filter[statuses][0][pipeline_id]' => $statuses_pipeline['pipeline_id'],
-                'limit' => 500,
-            ];
-    
-            if($statuses_pipeline['pipeline_id'] != 3987300)
-                $query = array_merge($query, ['filter[responsible_user_id]' => $responsible_user_id]);
-            
-            $leads = $amoApi->service->ajax()->get('/api/v4/leads', $query);
-            
-            if($leads !== true ) {
-    
-                $arrayLeads = array_merge($arrayLeads, $leads->_embedded->leads);
-                
-                if($leads->_page > 1) {
-                    
-                    for($i = 2; $i < $leads->_page; $i++) {
-    
-                        if($statuses_pipeline['status_id'] == '') continue;
-    
-                        $query = array_merge($query, ['page' => $i]);
-    
-                        if($statuses_pipeline['pipeline_id'] != 3987300)
-                            $query = array_merge($query, ['filter[responsible_user_id]' => $responsible_user_id]);
-    
-                        $leads = $amoApi->service->ajax()->get('/api/v4/leads', $query);
-    
-                        if($leads !== true ) {
-                            
-                            $arrayLeads = array_merge($arrayLeads, $leads->_embedded->leads);
-                        }
-                    }
-                }
-            }
-        }
-
-        if(count($arrayLeads) > 0) {
-    
-            Log::info(__METHOD__.' staff получил '.count($arrayLeads).' сделок для перевода на админа');
-    
-            return (object)$arrayLeads;
-            
-        } else
-            return null;
+        return $leads;
     }
 
     public static function create($contact, array $params, string $leadname)
@@ -116,28 +60,28 @@ abstract class Leads
     public static function update($lead, array $params, array $fields)
     {
         try {
-            
+
             if($fields) {
-    
+
                 foreach ($fields as $key => $field) {
-    
+
                     $lead->cf($key)->setValue($field);
                 }
             }
-    
+
             if(!empty($params['responsible_user_id']))
                 $lead->responsible_user_id = $params['responsible_user_id'];
-    
+
             if(!empty($params['status_id']))
                 $lead->status_id = $params['status_id'];
-    
+
             $lead->updated_at = time() + 2;
             $lead->save();
-    
+
             return $lead;
-        
+
         } catch (\Exception $exception) {
-            
+
             Log::error(__METHOD__. ' : ошибка обновления '.$exception->getMessage(). ' , сделка : '.$lead->id);
         }
     }
@@ -145,15 +89,15 @@ abstract class Leads
     public static function get($client, $id)
     {
         try {
-            
+
             $lead = $client->service->leads()->find($id);
-    
+
             return $lead;
-            
+
         } catch (\Exception $exception) {
-            
+
             sleep(2);
-            
+
             Log::error(__METHOD__. ' : '.$exception->getMessage(). ' , сделка : '.$id);
         }
     }
